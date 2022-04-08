9295b3291921034e16654e03f7dbe4b26b061900
jminchezm <jminchezm@miumg.edu.gt>
2022-04-07 22:39:31 -0600

    2da prueba jminchezm

--- FeLogin/inicio.h
+++ FeLogin/inicio.h
@@ -34,6 +34,8 @@ namespace FeLogin {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,18 +50,32 @@ namespace FeLogin {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(271, 111);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(295, 131);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"GRUPO 8";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// inicio
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(777, 462);
+			this->Controls->Add(this->button1);
 			this->Name = L"inicio";
 			this->Text = L"inicio";
+			this->Load += gcnew System::EventHandler(this, &inicio::inicio_Load);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+	private: System::Void inicio_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
